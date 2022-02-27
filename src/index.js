d887f8fb81bebf04bb362ab1a62bda89eb67b050
ddppddpp <djidji.perroto@gmail.com>
2022-02-27 23:15:23 +0200

    bg Translation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './i18n';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
