ec7d5aef0895018fa74b44d5bde22c09942b4365
DD PP <45116849+ddppddpp@users.noreply.github.com>
2022-02-28 11:13:08 +0200

    instana

---
+++ precache-manifest.4566543470bdff54113f3c57b7c7eecc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508b6b48748a9fa3b5ba6421f63151dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2193e100958da28809f",
+    "url": "/static/css/3.a0e9e383.chunk.css"
+  },
+  {
+    "revision": "d1152eda4d4021c9bdf8",
+    "url": "/static/css/main.6e23bca0.chunk.css"
+  },
+  {
+    "revision": "d2193e100958da28809f",
+    "url": "/static/js/3.a50dc0e2.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/3.a50dc0e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb9d8c8b0732a6b8bd2",
+    "url": "/static/js/4.93b2b831.chunk.js"
+  },
+  {
+    "revision": "1abed6f3fe0380333dc1",
+    "url": "/static/js/FastingAPIResponse.5ff72bd0.chunk.js"
+  },
+  {
+    "revision": "d1152eda4d4021c9bdf8",
+    "url": "/static/js/main.b48bcd36.chunk.js"
+  },
+  {
+    "revision": "4e9b71b2b9e9134760da",
+    "url": "/static/js/runtime-main.6fca8708.js"
+  }
+]);
